69d021f0b7c30f2dc671a67daa9f4bc9b05dc9a1
cmassera <cmassera@student.42angouleme.fr>
2023-11-08 15:00:47 +0100

    cSplit clean

--- .obj/Mandatory/main.d
+++
@@ -1,7 +0,0 @@
-.obj/Mandatory/main.o: Mandatory/main.cpp \
-  Mandatory/Class/PendingAction/PendingAction.cpp \
-  Mandatory/Class/PendingAction/PendingAction.hpp
-
-Mandatory/Class/PendingAction/PendingAction.cpp:
-
-Mandatory/Class/PendingAction/PendingAction.hpp:
