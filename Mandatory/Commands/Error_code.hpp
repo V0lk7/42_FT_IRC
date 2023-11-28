c4276396df2cf8746b53aeef086404342afd46f0
V0lk7 <j.duval7@protonmail.com>
2023-11-28 10:01:16 +0100

    Need to add err and rpl of privmsg

--- Mandatory/Commands/Error_code.hpp
+++ Mandatory/Commands/Error_code.hpp
@@ -33,6 +33,10 @@ JOIN{
 	ERR_CHANNELISFULL 471 //when a client try to join a full channel.
 	ERR_BADCHANNELKEY 475 //when a client try to join a channel with/without the good key.
 	ERR_NOTREGISTERED 451 // when a client isn't registered yet and try to do a command.
+	ERR_USERONCHANNEL 443 // when a user is already in the channel
+	
+	RPL_TOPIC 332 // if a channel topic is set
+	RPL_NOTOPIC 331 // if a channel topic isn't set
 }
 
 MODE{
@@ -46,6 +50,38 @@ MODE{
 	ERR_NOSUCHNICK 401// when user targeted doesn't exist
 	
 	RPL_CHANNELMODEIS 324 //when sending the mode active on the channel
+}
+
+TOPIC{
+	ERR_NEEDMOREPARAMS 461
+	ERR_CHANOPRIVSNEEDED 481
+	ERR_NOTONCHANNEL 442
+	RPL_TOPIC 332 // if a channel topic is set
+	RPL_NOTOPIC 331 // if a channel topic isn't set
+}
+
+INVITE{
+	ERR_NEEDMOREPARAMS 461
+	ERR_NOTONCHANNEL 442
+	ERR_NOSUCHCHANNEL 403 //channel not found.
+	ERR_CHANOPRIVSNEEDED 481
+	ERR_NOSUCHNICK 401// when user targeted doesn't exist
+	ERR_USERONCHANNEL 443 // when a user is already in the channel
+	
+	RPL_INVITING 341 // sending it to the client inviting the other to notice
+					 //	that the invit has be registered and send to the target
+}
+
+KICK{
+	ERR_NEEDMOREPARAMS 461
+	ERR_NOTONCHANNEL 442
+	ERR_NOSUCHCHANNEL 403 //channel not found.
+	ERR_CHANOPRIVSNEEDED 481
+}
+
+PRIVMSG{
+	ERR_NORECIPIENT 441 //Pas de destinataire donne
+	ERR_CANNOTSENDTOCHAN 404
 }
 
 #endif
