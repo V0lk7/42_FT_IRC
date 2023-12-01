8e30b98e3e1b72f410545b2a43a7a6c20987960c
Jimmy Mennock <jmennock@3B5.42angouleme.fr>
2023-12-01 08:32:25 +0100

    BUGS FOUND FIXED
    -> Changing the name of the Test channel to #Test caused a lot of problems. These have been corrected.
    -> The new management for finding the channel name also caused these problems. These have been corrected.
    -> Ready for revisions

--- Unitary-Test/Features/Topic/test.cpp
+++ Unitary-Test/Features/Topic/test.cpp
@@ -9,48 +9,48 @@
 TEST_CASE ( "topicParsing" ) 
 {
     Server                      server;
-    Channel*                    channel = server.GetChannel( "Test" );
+    Channel*                    channel = server.GetChannel( "#Test" );
     Client*                     sudo = server.GetClient( "sudo" );
     Client*                     Jean = server.GetClient( "Jean" );
     std::vector<std::string>    data;
     SUBCASE ( "true" )
     {
         data.push_back("#Test");
-        CHECK(topicParsing( data, *sudo, channel ) == TOPICNONE );
+        CHECK(topicParsing( *sudo, channel ) == TOPICNONE );
     }
     SUBCASE ( "no channel" )
     {
         data.push_back("#test");
-        CHECK(topicParsing( data, *sudo, channel ) != TOPICNONE );
+        CHECK(topicParsing( *sudo, NULL ) != TOPICNONE );
     }
     SUBCASE ( "Channel right but no right user" )
     {
         data.push_back("#Test");
-        CHECK(topicParsing( data, *Jean, channel ) == TOPICNONE );
+        CHECK(topicParsing( *Jean, channel ) == TOPICNONE );
     }
     SUBCASE ( "TOPIC_CHANGE_SET false but no right user" )
     {
         data.push_back("#Test");
-        CHECK(topicParsing( data, *Jean, channel ) == TOPICNONE );
+        CHECK(topicParsing( *Jean, channel ) == TOPICNONE );
     }
     SUBCASE ( "TOPIC_CHANGE_SET true but no right user" )
     {
         data.push_back("#Test");
         channel->SetMode( TOPIC_CHANGE_SET, true );
-        CHECK(topicParsing( data, *Jean, channel ) != TOPICNONE );
+        CHECK(topicParsing( *Jean, channel ) != TOPICNONE );
     }
     SUBCASE ( "Channel TOPIC_CHANGE_SET true but right user ok" )
     {
         data.push_back("#Test");
         channel->SetMode( TOPIC_CHANGE_SET, true );
-        CHECK( topicParsing( data, *sudo, channel ) == TOPICNONE );
+        CHECK( topicParsing( *sudo, channel ) == TOPICNONE );
     }
 }
 
 TEST_CASE ( "topicParsing" )
 {
     Server                      server;
-    Channel*                    channel = server.GetChannel( "Test" );
+    Channel*                    channel = server.GetChannel( "#Test" );
     Client*                     sudo = server.GetClient( "sudo" );
     Client*                     Jean = server.GetClient( "Jean" );
     std::vector<std::string>    data;
