c1f8092a4ad8ea73906b65479310f7e55ce990f9
Jimmy Mennock <jmennock@3B5.42angouleme.fr>
2023-12-06 14:42:01 +0100

    NE PAS FAIRE COMME GUILLAUME

--- Mandatory/Parsing/Parsing.cpp
+++ Mandatory/Parsing/Parsing.cpp
@@ -1,7 +1,9 @@
 #include "Tools.hpp"
 #include "Client.hpp"
 #include "Server.hpp"
+#include "Commands.hpp"
 
+#include <cctype>
 #include <strings.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -28,6 +30,7 @@
 // ########################################################################## //
 // #~TODO MANAGER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# //
 // Still to make :
+// -> Management of NickName
 // Error found :
 //      SEGFAULT on empty data after all keyword
 // ########################################################################## //
@@ -45,22 +48,25 @@
 // USER tdc
 // ########################################################################## //
 
-// static void
-// dispatch( const std::string& info, int& way, Client& person, const Server& server );
 static void
-ncCreation( const std::vector<std::string>& info, Client& person, const Server& server );
+dispatch( std::string& info, int& way, Client& person, Server& server );
 static void
-userCreation( const std::vector<std::string>& info, Client& person, const Server& server );
+ncCreation( const std::vector<std::string>& info, Client& person, Server& server );
+static void
+userCreation( const std::vector<std::string>& info, Client& person, Server& server );
 static int
 wayChooser( const std::string& target );
 static void
-loadUserData( Client& person, const Server& server, const std::string& data, int word );
+loadUserData( Client& person, Server& server, const std::string& data, int word );
+static bool
+isValidNickName( Server& server, std::string key );
+static void
+parsingReaply( Client& person, int flag );
 
 // ########################################################################## //
 // #_PARSER COMMAND_________________________________________________________# //
-
 int
-handleCommand( const char* buffer, const Server& server, Client& person ) {
+handleCommand( const char* buffer, Server& server, Client& person ) {
     std::string                 work;
     std::vector<std::string>    tab;
     std::vector<std::string>    line;
@@ -78,10 +84,10 @@ handleCommand( const char* buffer, const Server& server, Client& person ) {
             way = wayChooser( line[0] );
         if ( way == CLIENT )
             break ;
-        // else if ( way != -1 && line.size() > 1 )
-        //      dispatch( tab[i], way, person, server );
-        // else
-        //      ERR_NEEDMOREPARAMS
+        else if ( way != -1 && line.size() > 1 )
+             dispatch( tab[i], way, person, server );
+        else
+             // ERR_NEEDMOREPARAMS;
         line.clear();                                                    // i need to split cmd one by one
     }
     if ( tab.size() == 1 && tab[0].find( "CAP" ) == std::string::npos )
@@ -92,37 +98,38 @@ handleCommand( const char* buffer, const Server& server, Client& person ) {
     return ( way );
 }
 
-// static void
-// dispatch( const std::string& info, int& way, Client& person, const Server& server ) {
-//     (void)info;
-//     (void)person;
-//     (void)server;
-// //     if ( !person.GetRight )
-// //          way = -1;
-//     switch ( way ) {
-//         // case TOPIC :
-//         // break ;
-//         // case INVITE :
-//         // break ;
-//         // case PRIVMSG :
-//         // break ;
-//         // case KICK :
-//         // break ;
-//         // case JOIN :
-//         // break ;
-//         // case MODE :
-//         // break ;
-//         default :
-// //             ERROR_444 error no login
-//             return ;
-//     }
-// }
+static void
+dispatch( std::string& info, int& way, Client& person, Server& server ) {
+    (void)info;
+    (void)person;
+    (void)server;
+    if ( !person.GetStatement() )
+         way = -1;
+    switch ( way ) {
+        // case TOPIC :
+            // break ;
+        // case INVITE :
+            // break ;
+        case PRIVMSG :
+            privateMessage( server, person, info );
+            break ;
+        // case KICK :
+            // break ;
+        case JOIN :
+            Join( server, person, info );
+            break ;
+        // case MODE :
+            // break ;
+        default :
+//             ERROR_444 error no login
+            return ;
+    }
+}
 
 // ########################################################################## //
 // #_CLIENT GESTION_________________________________________________________# //
-
 static void
-ncCreation( const std::vector<std::string>& info, Client& person, const Server& server ) {
+ncCreation( const std::vector<std::string>& info, Client& person, Server& server ) {
     std::vector<std::string>    toParse;
     std::vector<std::string>    word;
     size_t                      check = 0;
@@ -140,8 +147,7 @@ ncCreation( const std::vector<std::string>& info, Client& person, const Server& 
             }
         }
         if ( check == toParse.size() )
-            send( person.GetSocket(), \
-                    "Require password\n", strlen( "Require password\n" ) + 1, 0 );
+            parsingReaply( person, sPASS );
         return ;
     }
     for ( size_t i = 0; i < toParse.size() && person.GetStatementStep( PASSWD ); i++ ) {
@@ -155,42 +161,41 @@ ncCreation( const std::vector<std::string>& info, Client& person, const Server& 
             return ;
         }
         else if ( !person.GetStatementStep( NICK ) ) {
-            send( person.GetSocket(), \
-                    "Require nickname\n", strlen( "Require nickname\n" ) + 1, 0 );
+            parsingReaply( person, sNICK );
             return ;
         }
     }
 
     if ( person.GetStatementStep( PASSWD ) && person.GetStatementStep( NICK ) \
         && !person.GetStatementStep( USER ) )
-        send( person.GetSocket(), \
-                "Require username\n", strlen( "Require username\n" ) + 1, 0 );
+        parsingReaply( person, sUSER );
 }
 
 static void
-userCreation( const std::vector<std::string>& info, Client& person, const Server& server ) {
+userCreation( const std::vector<std::string>& info, Client& person, Server& server ) {
     std::vector<std::string>    toParse;
     std::vector<std::string>    word;
-
-    std::cout << "\tUSERCREATION\n";
 
     if ( info.empty() ) {
         return ;
     }
 
     toParse = info;
-    for ( size_t i = 0; i < toParse.size(); i++ ) {
+    for ( size_t i = 0; i < toParse.size(); i++ )
+    {
         if ( toParse[i].find( "PASS" ) != std::string::npos )
-                loadUserData( person, server, toParse[i], sPASS );
+            loadUserData( person, server, toParse[i], sPASS );
+
         else if ( toParse[i].find( "NICK" ) != std::string::npos )
             loadUserData( person, server, toParse[i], sNICK );
+
         else if ( toParse[i].find( "USER" ) != std::string::npos )
             loadUserData( person, server, toParse[i], sUSER );
     }
 }
 
 static void
-loadUserData( Client& person, const Server& server, const std::string& data, int word ) {
+loadUserData( Client& person, Server& server, const std::string& data, int word ) {
     std::string::size_type      it;
     std::string                 toWork( data );
     std::string                 buffer;
@@ -199,29 +204,43 @@ loadUserData( Client& person, const Server& server, const std::string& data, int
     if ( word == sNICK ) {
         it = toWork.find( "NICK" );
         buffer = toWork.substr( it + 5, std::string::npos );
-        // TODO manage empty after NICK
-        if ( buffer.size() < 9 )
+        if ( isValidNickName(server, buffer) )
             person.SetNickname( buffer );
         else
-            person.SetNickname( "ERROR" ); // TODO ERROR gestion need better gestion
+            parsingReaply( person, sNICK );
         buffer.clear();
     }
     else if ( word == sPASS ) {
         it = toWork.find( "PASS" );
-        // TODO manage empty after PASS
         buffer = toWork.substr( it + 5, std::string::npos );
         if ( server.GetPassword() == buffer )
             person.SetPasswd();
         else
-            send( person.GetSocket(), \
-                    "Require password\n", strlen( "Require password\n" ) + 1, 0 );
+            parsingReaply( person, sPASS );
         buffer.clear();
     }
     else if ( word == sUSER ) {
         tab = split( data, " " );
-        // TODO manage empty after USER
-        person.SetUsername( tab[1] );
+        if ( tab.size() > 1 ) 
+            person.SetUsername( tab[1] );
     }
+}
+
+static bool
+isValidNickName( Server& server, std::string key )
+{
+    if ( key.length() < 9 )
+        return ( false );
+
+    for( size_t i = 0; i < key.length(); i++) {
+        if ( !std::isalnum( key[i] ) )
+            return ( false );
+    }
+
+    if ( server.GetClient( key ) )
+        return ( false );
+
+    return ( true );
 }
 
 static int
@@ -242,28 +261,20 @@ wayChooser( const std::string& target ) {
     return ( CLIENT );
 }
 
-// ########################################################################## //
-// #~TESTING TOOL~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# //
+static void
+parsingReaply( Client& person, int flag )
+{
+	std::string	reply;
 
-// static void
-// print( std::string in, std::vector<std::string>& tab ) {
-//     int i = 0;
-//     std::cout << in << ":" << std::endl;
-//     for ( std::vector<std::string>::iterator it = tab.begin(); it < tab.end(); it++, i++ )
-//         std::cout << "tab[" << i << "]: " << *it << std::endl;
-//     std::cout << std::endl;
-// }
+    if ( flag  == sPASS ) {
+        reply = "Require password.\r\n";
+    }
+    else if ( flag == sUSER ) {
+        reply = "Require username.\r\n";
+    }
+    else if ( flag == sNICK ) {
+        reply = "Require nickname.\r\n";
+    }
 
-// static void
-// printClient( const Client& person ) {
-//     std::cout << "NickName: " << person->GetNickname() << std::endl;
-//     std::cout << "User: " << person->GetUsername() << std::endl;
-
-// void
-// test( Client& person ) {
-//     std::string test = "Ceci est un test\r\nTEST\n";
-//     person.SetInputBuffer( test );
-//     std::cout << "Inside Client: " << person.GetInputBuffer() << std::endl;
-// }
-// }
-// ########################################################################## //
+    person.SetMessageToSend( reply );
+}
